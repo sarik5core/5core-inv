50f2aa1e184777cc63a278666560aed04426b54b
Riddhi123 <riddhi.mesari@gmail.com>
2025-09-20 01:12:57 +0530

    Update OutgoingController.php

--- app/Http/Controllers/InventoryManagement/OutgoingController.php
+++ app/Http/Controllers/InventoryManagement/OutgoingController.php
@@ -19,8 +19,8 @@ class OutgoingController extends Controller
 {
 
     protected $shopifyDomain = '5-core.myshopify.com';
-    protected $shopifyApiKey = '01f70fee8001931b5a25e3df24d6d749';
-    protected $shopifyPassword = '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';
+    protected $shopifyApiKey = '818a43f8d3ae2f7bcfed50abb1e6deb9';
+    protected $shopifyPassword = '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';
 
     protected $apiController;
 
